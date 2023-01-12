aff463e8ad90d9dd70a0e02d33e5a479740d8c51
Train XIII <116897631+MohammedIsamAli@users.noreply.github.com>
2023-01-12 13:20:28 +0200

    Create 3.cpp

---
+++ From 9 - 15 lessons/3.cpp
@@ -0,0 +1,16 @@
+/*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ invalid ] => start with number ?
+  __name      [ valid ] => bad practice ?
+  name@name   [ valid ] => good practice ?
+  name10name  [ valid ] => good practice ?
+  name!name   [ valid ] => good p?
+  first_NAME  [ valid ] => good practice
+  first_name  [ valid ] => good practice ?
+  firstName   [ valid ] => good practice ?
+  first name  [ invalid ] => there is a space ?
+  fn          [ valid ] => Reason ?
+  public      [invalid ] => c++ keyword ?
+  Public      [ valid ] => bad practice ?
+*/
